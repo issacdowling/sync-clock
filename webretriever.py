5626eb70e7ea89bca826e78df854f070609a519f
Issac Dowling <12268666-issacdowling@users.noreply.gitlab.com>
2022-11-07 23:11:32 +0000

    Add checking of whether timer is running

--- webretriever.py
+++ webretriever.py
@@ -1,7 +1,15 @@
 import requests
 import time
 
-while True:
-    timersecs = requests.get("http://localhost:5000/timer")
-    print(timersecs.json()["seconds"])
-    time.sleep(1)
+timerWanted = False
+
+if input("Do you want to get the timer? [Y/n]") != "n":
+    timer = requests.get("http://localhost:5000/timer")
+    if timer.json()["running"] == "True":
+        while True:
+            timer = requests.get("http://localhost:5000/timer")
+            print(timer.json()["seconds"])
+            time.sleep(1)
+    else:
+        print("Well, there's no timer running.")
+        exit()
