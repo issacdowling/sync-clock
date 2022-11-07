e21d3a608a88cc4c2de2954a10d7756c45085ff7
Issac Dowling <12268666-issacdowling@users.noreply.gitlab.com>
2022-11-07 23:55:45 +0000

    Work on dismissing timer and notifying when done

--- webretriever.py
+++ webretriever.py
@@ -5,9 +5,14 @@ timerWanted = False
 
 if input("Do you want to get the timer? [Y/n]") != "n":
     timer = requests.get("http://localhost:5000/timer").json()
-    if timer["running"] == True:       
+    if timer["running"] == True:
         while True:
             timer = requests.get("http://localhost:5000/timer").json()
+            if timer["running"] == False:
+                if timer["dismissed"] == False:
+                    print("Timer done")
+                else:
+                    exit("Dismissed")
             print(timer["seconds"])
             time.sleep(1)
     else:
