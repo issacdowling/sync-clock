78bd092d4d6314f9eb0f9dc22b5e8a475d3382ba
Issac Dowling <12268666-issacdowling@users.noreply.gitlab.com>
2022-11-07 23:53:34 +0000

    Do Json better

--- webretriever.py
+++ webretriever.py
@@ -4,11 +4,11 @@ import time
 timerWanted = False
 
 if input("Do you want to get the timer? [Y/n]") != "n":
-    timer = requests.get("http://localhost:5000/timer")
-    if timer.json()["running"] == True:
+    timer = requests.get("http://localhost:5000/timer").json()
+    if timer["running"] == True:       
         while True:
-            timer = requests.get("http://localhost:5000/timer")
-            print(timer.json()["seconds"])
+            timer = requests.get("http://localhost:5000/timer").json()
+            print(timer["seconds"])
             time.sleep(1)
     else:
         print("Well, there's no timer running.")
