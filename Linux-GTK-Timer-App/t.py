072a9a413a50aca0823e99cebe3cb35ea0469284
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2022-11-12 00:01:50 +0000

    Allow access from remote machines

--- Linux-GTK-Timer-App/t.py
+++ Linux-GTK-Timer-App/t.py
@@ -8,7 +8,7 @@ gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw, GLib
 import json
 
-URL = "http://localhost:4761"
+URL = "http://10.0.0.3:4761"
 
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
--- webserver.py
+++ webserver.py
@@ -40,4 +40,4 @@ def balls():
     return "balls"
 
 if __name__ == "__main__":
-    app.run(port=4761)
+    app.run(host='0.0.0.0', port=4761)
