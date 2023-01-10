0c31dc87fe8c3990413ea32020bdd3e809d82048
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2023-01-10 12:55:24 +0000

    nvm no androidjs

--- Android-Timer-Client/Timer/main.js
+++
@@ -1,9 +0,0 @@
-const back = require('androidjs').back;
-
-back.on("hello from front", function(){
-	back.send("hello from back", "Hello from Android JS");
-});
-
-function send_toggle_timer(){
-	app.toast.show("Hello Android JS", 1);
-}
