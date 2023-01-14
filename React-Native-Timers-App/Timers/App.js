0302a803091f46fa56013ab3bb64c6ce944086ff
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2023-01-14 21:31:31 +0000

    test

--- React-Native-Timers-App/Timers/App.js
+++ React-Native-Timers-App/Timers/App.js
@@ -9,13 +9,13 @@ export default function App() {
   const ip = "10.0.0.20"
   let socket = new WebSocket("ws://" + ip + ":4762/timer");
   let timer_is_running = false;
-  const source = "Phone"
+  const source = "Phone";
 
   // Set state for the text input field
   const [inputtedTimerLength, setInputtedTimerLength] = useState('');
 
   // Set state for the timer output string
-  const [timerDisplayString, setTimerDisplayString] = useState('')
+  const [timerDisplayString, setTimerDisplayString] = useState('1')
 
   // When the connection is opened:
   socket.onopen = function(e) {
@@ -30,12 +30,41 @@ export default function App() {
 
   //Sends the JSON for stopping the timer
   function sendStopTimer() {
-    socket.send(JSON.stringify({"stop" : "please"}))
+    socket.send(JSON.stringify({"stop" : "please"}));
   }
 
   function sendStartTimer() {
     socket.send(JSON.stringify({"length" : Number(inputtedTimerLength), "source" : source}))
   }
+
+  // When the connection recieves a message:
+socket.onmessage = function(event) {
+  let length;
+  let recieved_json;
+  console.log(`[message] Data received from server: ${event.data}`);
+  recieved_json = JSON.parse(event.data)
+
+  //If timer-related
+  if ("remaining_length" in recieved_json) {
+
+      // Convert seconds into hours:minutes:seconds
+      let date = new Date(null);
+      date.setSeconds(recieved_json["remaining_length"]);
+      length = date.toISOString().substr(11, 8);
+      // Checks if hours are empty, shorten if so.
+      if (length.slice(0,3) == "00:") {
+          length = length.slice(3)
+      }
+
+      //Set the div to the time remaining
+      setTimerDisplayString(length);
+
+      }
+      
+  }
+  
+  //document.querySelector("#debug").innerHTML = event.data
+
 
   return (
     <View style={styles.container}>
@@ -45,8 +74,9 @@ export default function App() {
         <Button title="STOP TIMER" onPress={sendStopTimer} style={styles.button}/>
       </View>
         <View>
-          <Text>{timerDisplayString}</Text>
+          <Text style={styles.timeLeftText}>{timerDisplayString}</Text>
         </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -66,6 +96,12 @@ const styles = StyleSheet.create({
   titleText: {
     color: 'white',
     padding: 16
+  },
+  timeLeftText: {
+    color: 'white',
+    padding: 16,
+    fontSize: 100,
+    textAlign: 'center'
   },
   inputText: {
     color: 'white',
